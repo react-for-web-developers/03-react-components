8415cdaa93f92c9ee3a1891522c83b89cfd4fbce
ibell1 <86377038+ibell1@users.noreply.github.com>
2021-10-19 00:01:09 -0400

    Create Repositories.jsx

---
+++ exercises/components/Repositories.jsx
@@ -0,0 +1,13 @@
+import Repo from './Repository';
+
+export function Repositories() {
+  return (
+    <>
+      <h3>This contains multiple repositories</h3>
+      <Repository />
+      <Repository />
+      <Repository />
+      <Repository />
+    </>
+  );
+}
